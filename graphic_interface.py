7f7cbceb15c64ae5f49b46a29f9781c32c82cc26
Papszi <szilagyipapdavid@gmail.com>
2022-04-18 20:26:03 +0300

    Commit - Code Format

--- graphic_interface.py
+++ graphic_interface.py
@@ -6,10 +6,12 @@ from tkinter import messagebox
 import minimax_algorithm
 from dataclasses import dataclass, replace
 
+
 @dataclass
 class Contestant:
     x: int
     y: int
+
 
 def game_over(game_array, contestant_pos):
     x = contestant_pos.x
@@ -17,28 +19,34 @@ def game_over(game_array, contestant_pos):
     array_length = len(game_array)
     ok = False
 
-    #we check whether there is a free position in the vicinity of our player
-    for i in range (-1, 2):
-        for j in range (-1,2):
+    # we check whether there is a free position in the vicinity of our player
+    for i in range(-1, 2):
+        for j in range(-1, 2):
             x_temp = x + i
             y_temp = y + j
-            if (x_temp>=0 and x_temp < array_length and y_temp < array_length and y_temp >= 0):
-                if (game_array[x_temp][y_temp]==0):
+            if (
+                x_temp >= 0
+                and x_temp < array_length
+                and y_temp < array_length
+                and y_temp >= 0
+            ):
+                if game_array[x_temp][y_temp] == 0:
                     ok = True
 
-    #if the human player is the one who loses, we return 1, if it's the bot, we return -1, and if it's not game over, we return 0  
-    if (ok == False):
-        if (game_array[contestant_pos.x][contestant_pos.y] == 2):
+    # if the human player is the one who loses, we return 1, if it's the bot, we return -1, and if it's not game over, we return 0
+    if ok == False:
+        if game_array[contestant_pos.x][contestant_pos.y] == 2:
             return 1
         else:
             return -1
     else:
         return 0
 
+
 def gameplay_initialiser(tile_nr_aux):
-    print(turn.get())
     tile_set(tile_nr_aux)
     map_initialiser()
+
 
 def tile_set(tile_nr_aux):
     global tile_nr, master
@@ -48,128 +56,156 @@ def tile_set(tile_nr_aux):
     master.columnconfigure(tile_nr, weight=1)
     master.rowconfigure(tile_nr, weight=1)
 
+
 def map_initialiser():
     global tile_list, tile_nr, master, game_array, player, bot, turn
     tile_list = [[0 for i in range(tile_nr)] for j in range(tile_nr)]
-    
-    for i in range (0,tile_nr):
-        for j in range (0,tile_nr):
-            tile_list[i][j] = (Button(master, text = "",bg='white', activebackground='white', width = 120//tile_nr, height = 45//tile_nr, command=lambda i=i, j=j:game_running(i,j)))
 
-    tile_list[0][tile_nr//2].config(bg='red', activebackground='red')
-    tile_list[tile_nr-1][tile_nr//2].config(bg='green', activebackground='green')
+    for i in range(0, tile_nr):
+        for j in range(0, tile_nr):
+            tile_list[i][j] = Button(
+                master,
+                text="",
+                bg="white",
+                activebackground="white",
+                width=120 // tile_nr,
+                height=45 // tile_nr,
+                command=lambda i=i, j=j: game_running(i, j),
+            )
 
-    print(len(tile_list))
+    tile_list[0][tile_nr // 2].config(bg="red", activebackground="red")
+    tile_list[tile_nr - 1][tile_nr // 2].config(bg="green", activebackground="green")
 
-    for i in range (0,tile_nr):
-        for j in range (0,tile_nr):
+    for i in range(0, tile_nr):
+        for j in range(0, tile_nr):
             tile_list[i][j].grid(row=i, column=j)
 
-    game_array = np.zeros((tile_nr,tile_nr))
-    game_array[tile_nr-1][tile_nr//2] = 2
-    game_array[0][tile_nr//2] = 3
-    player = replace(player, x = tile_nr-1, y = tile_nr//2)
-    bot = replace(bot, x=0, y=tile_nr//2)
-    if (turn.get()==2): #We call game_running so the bot can make its first step
-        game_running(0,0)
+    game_array = np.zeros((tile_nr, tile_nr))
+    game_array[tile_nr - 1][tile_nr // 2] = 2
+    game_array[0][tile_nr // 2] = 3
+    player = replace(player, x=tile_nr - 1, y=tile_nr // 2)
+    bot = replace(bot, x=0, y=tile_nr // 2)
+    if turn.get() == 2:  # We call game_running so the bot can make its first step
+        game_running(0, 0)
+
 
 def player_position(game_array):
     array_length = len(game_array)
 
     for i in range(0, array_length):
-        for j in range (0, array_length):
-                if game_array[i][j] == 2:
-                    player_pos = (i,j)
+        for j in range(0, array_length):
+            if game_array[i][j] == 2:
+                player_pos = (i, j)
 
     return player_pos
+
 
 def check_winner(contestant_pos):
     ok = game_over(game_array, contestant_pos)
 
-    if (ok == 1):
-        print("THE BOT HAS WON")
+    if ok == 1:
         messagebox.showinfo("Game Over", "The Bot has won")
         quit()
     else:
-        if (ok == -1):
-            print("THE PLAYER HAS WON")
+        if ok == -1:
             messagebox.showinfo("Game Over", "You have won")
             quit()
     return 0
 
-def game_running(i,j):
+
+def game_running(i, j):
     global turn, player, bot
     contestant_pos = player_position(game_array)
 
-    if (turn.get() == 0):
-        if (game_array[i][j]==0 and abs(i - contestant_pos[0]) <= 1 and abs(j - contestant_pos[1]) <= 1 ):
-            graphic_player_move(i,j)
+    if turn.get() == 0:
+        if (
+            game_array[i][j] == 0
+            and abs(i - contestant_pos[0]) <= 1
+            and abs(j - contestant_pos[1]) <= 1
+        ):
+            graphic_player_move(i, j)
             turn.set(1)
     else:
-        if (turn.get() == 1):
-            if (game_array[i][j]==0):
-                graphic_player_block(i,j)
+        if turn.get() == 1:
+            if game_array[i][j] == 0:
+                graphic_player_block(i, j)
                 turn.set(2)
-            
-        if (check_winner(bot) != 0):
+
+        if check_winner(bot) != 0:
             return
 
-        if (turn.get() == 2):
-            graphic_bot_move(tile_nr)            
-            if (check_winner(player) != 0):
+        if turn.get() == 2:
+            graphic_bot_move(tile_nr)
+            if check_winner(player) != 0:
                 return
             turn.set(0)
+
 
 def graphic_bot_move(tile_nr):
     global player, bot
     game_array_copy = copy.deepcopy(game_array)
-    print("bot",game_array)
-    if (tile_nr>3):
-        print("Heuristical minimax")
-        optimal_pos = minimax_algorithm.minimax_heuristic(game_array_copy, "maxi", 0, -100, 100)  
+    if tile_nr > 3:
+        optimal_pos = minimax_algorithm.minimax_heuristic(
+            game_array_copy, "maxi", 0, -100, 100
+        )
     else:
-        print("Plain minimax")
         optimal_pos = minimax_algorithm.minimax(game_array_copy, "maxi", 0)
 
     game_array[bot.x][bot.y] = 0
-    tile_list[bot.x][bot.y].config(bg='white', activebackground='white')
-    print("optimal_pos: ", optimal_pos)
+    tile_list[bot.x][bot.y].config(bg="white", activebackground="white")
     new_bot_pos = optimal_pos[0]
     blocking_pos = optimal_pos[1]
     minimax_algorithm.dynamic_depth = minimax_algorithm.dynamic_depth + 1
-    
+
     game_array[new_bot_pos[0]][new_bot_pos[1]] = 3
     game_array[blocking_pos[0]][blocking_pos[1]] = 1
-    bot = replace(bot, x = new_bot_pos[0], y = new_bot_pos[1])
+    bot = replace(bot, x=new_bot_pos[0], y=new_bot_pos[1])
 
-    tile_list[bot.x][bot.y].config(bg="red", activebackground='red')
-    tile_list[blocking_pos[0]][blocking_pos[1]].config(bg='grey', activebackground='grey')
+    tile_list[bot.x][bot.y].config(bg="red", activebackground="red")
+    tile_list[blocking_pos[0]][blocking_pos[1]].config(
+        bg="grey", activebackground="grey"
+    )
 
     return 1
-    
-def graphic_player_move(i,j):
 
-    tile_list[player.x][player.y].config(bg='white', activebackground='white')
+
+def graphic_player_move(i, j):
+
+    tile_list[player.x][player.y].config(bg="white", activebackground="white")
     game_array[player.x][player.y] = 0
     player.x = i
     player.y = j
     game_array[i][j] = 2
-    tile_list[i][j].config(bg='green', activebackground='green')
-    print("JMozdulat1",game_array)
+    tile_list[i][j].config(bg="green", activebackground="green")
 
-def graphic_player_block(i,j):
+
+def graphic_player_block(i, j):
     game_array[i][j] = 1
-    tile_list[i][j].config(bg='grey', activebackground='grey')
+    tile_list[i][j].config(bg="grey", activebackground="grey")
 
 
 def interface_menu():
     global turn
     master.geometry("1100x1000")
     master.title("Tic Tac Toe Advanced")
-    button_mode3 = Button(master, text = "3x3",width = 20, height = 10, command=lambda:gameplay_initialiser(3))
-    button_mode5 = Button(master, text = "5x5",width = 20, height = 10, command=lambda:gameplay_initialiser(5))
-    who_starts = Checkbutton(master, text = "The bot should start", variable=turn, onvalue=2, offvalue=0, indicatoron=True)
-    tutorial_text = Label(master, text = "\nThe game is played as follows:\n\n 1. The player needs to pick a surrounding tile that he wants to step into \n(the player can move diagonally, horizontally or vertically) \n\n2. The player also needs to block a tile, this tile can be any tile on the map, as long as it's a free tile. \n\nThe win condition is when your opponent can no longer step anywhere. \n\nColours:\nGreen - You, the player\nRed - The bot\nGrey - blocked tile\nWhite - free tile")
+    button_mode3 = Button(
+        master, text="3x3", width=20, height=10, command=lambda: gameplay_initialiser(3)
+    )
+    button_mode5 = Button(
+        master, text="5x5", width=20, height=10, command=lambda: gameplay_initialiser(5)
+    )
+    who_starts = Checkbutton(
+        master,
+        text="The bot should start",
+        variable=turn,
+        onvalue=2,
+        offvalue=0,
+        indicatoron=True,
+    )
+    tutorial_text = Label(
+        master,
+        text="\nThe game is played as follows:\n\n 1. The player needs to pick a surrounding tile that he wants to step into \n(the player can move diagonally, horizontally or vertically) \n\n2. The player also needs to block a tile, this tile can be any tile on the map, as long as it's a free tile. \n\nThe win condition is when your opponent can no longer step anywhere. \n\nColours:\nGreen - You, the player\nRed - The bot\nGrey - blocked tile\nWhite - free tile",
+    )
     button_mode3.pack()
     button_mode5.pack()
     who_starts.pack()
@@ -177,11 +213,11 @@ def interface_menu():
     master.mainloop()
 
 
-tile_nr=0
+tile_nr = 0
 master = Tk()
 turn = IntVar()
 game_array = 0
-player = Contestant(0,0)
-bot = Contestant(0,0)
+player = Contestant(0, 0)
+bot = Contestant(0, 0)
 
 interface_menu()
--- minimax_algorithm.py
+++ minimax_algorithm.py
@@ -10,16 +10,18 @@ from tkinter import messagebox
 from dataclasses import dataclass, replace
 
 
-#As the map becomes bigger, we need to set a dynamic depth, so at the start of the game we might only check the first 2 levels
-#but as the game progresses (and the tree of possible choices decreases) we will go deeper 
+# As the map becomes bigger, we need to set a dynamic depth, so at the start of the game we might only check the first 2 levels
+# but as the game progresses (and the tree of possible choices decreases) we will go deeper
 dynamic_depth = 0
+
 
 @dataclass
 class StepBlock:
-    step_x : int
-    step_y : int
-    block_x : int
-    block_y : int
+    step_x: int
+    step_y: int
+    block_x: int
+    block_y: int
+
 
 def occupiable_spaces(game_array, contestant_pos):
     list_occupiable = []
@@ -27,15 +29,21 @@ def occupiable_spaces(game_array, contestant_pos):
     y = contestant_pos[1]
     array_length = len(game_array)
 
-    #we check the spaces next to the contestant, and put the places he can step into
-    for i in range (-1, 2):
-        for j in range (-1,2):
+    # we check the spaces next to the contestant, and put the places he can step into
+    for i in range(-1, 2):
+        for j in range(-1, 2):
             x_temp = x + i
             y_temp = y + j
-            if (x_temp>=0 and x_temp < array_length and y_temp < array_length and y_temp >= 0):
-                if (game_array[x_temp][y_temp]==0):
+            if (
+                x_temp >= 0
+                and x_temp < array_length
+                and y_temp < array_length
+                and y_temp >= 0
+            ):
+                if game_array[x_temp][y_temp] == 0:
                     list_occupiable.append((x_temp, y_temp))
-    return list_occupiable   
+    return list_occupiable
+
 
 def occupiable_spaces_degree(game_array, contestant_pos):
     degree_free_pos = []
@@ -43,172 +51,192 @@ def occupiable_spaces_degree(game_array, contestant_pos):
     y = contestant_pos[1]
     array_length = len(game_array)
 
-    #We iterate through every possible position the contestant can step into, and we give each one a degree based on how many possible positions
-    #we could further step into from there
+    # We iterate through every possible position the contestant can step into, and we give each one a degree based on how many possible positions
+    # we could further step into from there
 
-    for i in range (-1, 2):
-        for j in range (-1,2):
+    for i in range(-1, 2):
+        for j in range(-1, 2):
             x_temp = x + i
             y_temp = y + j
-            if (x_temp>=0 and x_temp < array_length and y_temp < array_length and y_temp >= 0):
-                if (game_array[x_temp][y_temp]==0):
-                    temp_list = occupiable_spaces(game_array, (x_temp,y_temp))
-                    degree_free_pos.append(len(temp_list)-1)
-    return degree_free_pos   
+            if (
+                x_temp >= 0
+                and x_temp < array_length
+                and y_temp < array_length
+                and y_temp >= 0
+            ):
+                if game_array[x_temp][y_temp] == 0:
+                    temp_list = occupiable_spaces(game_array, (x_temp, y_temp))
+                    degree_free_pos.append(len(temp_list) - 1)
+    return degree_free_pos
+
 
 def positions(game_array):
-    #We iterate through the whole table searching for the bot and player's position
+    # We iterate through the whole table searching for the bot and player's position
     array_length = len(game_array)
     for j in range(0, array_length):
-        for i in range (0, array_length):
+        for i in range(0, array_length):
             if game_array[i][j] == 2:
-                player_pos = (i,j)
+                player_pos = (i, j)
             else:
                 if game_array[i][j] == 3:
-                    bot_pos = (i,j)
+                    bot_pos = (i, j)
     return player_pos, bot_pos
 
+
 def blockable_pos(game_array):
-    #we iterate through the whole table searching for the tiles that the contestant can block
+    # we iterate through the whole table searching for the tiles that the contestant can block
     array_length = len(game_array)
     list_blockable = []
     for i in range(0, array_length):
-        for j in range (0, array_length):
+        for j in range(0, array_length):
             if game_array[i][j] == 0:
-                list_blockable.append((i,j))
+                list_blockable.append((i, j))
     return list_blockable
 
-def minimax(game_array, level, nr):
-    player_pos, bot_pos = positions(game_array) 
-    step_and_block = StepBlock(0,0,0,0)
 
-    if (level == "maxi"):
+def minimax(game_array, level, nr):
+    player_pos, bot_pos = positions(game_array)
+    step_and_block = StepBlock(0, 0, 0, 0)
+
+    if level == "maxi":
         contestant_steppable = occupiable_spaces(game_array, bot_pos)
         val = -10
         for i in contestant_steppable:
 
-            game_array[bot_pos[0]][bot_pos[1]] = 0  # the contestant's original position becomes zero and we select the next one
-            game_array[i[0]][i[1]] = 3 
-            contestant_blockable = blockable_pos(game_array) 
+            game_array[bot_pos[0]][
+                bot_pos[1]
+            ] = 0  # the contestant's original position becomes zero and we select the next one
+            game_array[i[0]][i[1]] = 3
+            contestant_blockable = blockable_pos(game_array)
 
             for j in contestant_blockable:
                 game_array[j[0]][j[1]] = 1
-                result = minimax(game_array, "mini", nr+1)
+                result = minimax(game_array, "mini", nr + 1)
                 game_array[j[0]][j[1]] = 0
-                if (val<result):
+                if val < result:
                     val = result
-                    #if we are at the last level, we save the winning position the bot should step into
-                    if (nr==0 and val >= 1):
+                    # if we are at the last level, we save the winning position the bot should step into
+                    if nr == 0 and val >= 1:
                         game_array[j[0]][j[1]] = 0
-                        game_array[i[0]][i[1]] = 0 
+                        game_array[i[0]][i[1]] = 0
                         game_array[bot_pos[0]][bot_pos[1]] = 3
-                        return (i[0],i[1]), (j[0],j[1])
-                
-            game_array[i[0]][i[1]] = 0 # we reset the positions to their initial values
+                        return (i[0], i[1]), (j[0], j[1])
+
+            game_array[i[0]][i[1]] = 0  # we reset the positions to their initial values
             game_array[bot_pos[0]][bot_pos[1]] = 3
 
-        if (nr==0):
-            return (i[0],i[1]),(j[0],j[1])
+        if nr == 0:
+            return (i[0], i[1]), (j[0], j[1])
         return val
     else:
-        if (level == "mini"):
+        if level == "mini":
             val = 10
             contestant_steppable = occupiable_spaces(game_array, player_pos)
             for i in contestant_steppable:
                 game_array[player_pos[0]][player_pos[1]] = 0
-                game_array[i[0]][i[1]] = 2 
-                contestant_blockable = blockable_pos(game_array) 
+                game_array[i[0]][i[1]] = 2
+                contestant_blockable = blockable_pos(game_array)
                 for j in contestant_blockable:
                     game_array[j[0]][j[1]] = 1
-                    result = minimax(game_array, "maxi", nr+1)
+                    result = minimax(game_array, "maxi", nr + 1)
                     game_array[j[0]][j[1]] = 0
                     val = min(val, result)
-                    if (val < -1):
+                    if val < -1:
                         game_array[j[0]][j[1]] = 0
-                        game_array[i[0]][i[1]] = 0 
+                        game_array[i[0]][i[1]] = 0
                         game_array[player_pos[0]][player_pos[1]] = 2
                         break
-                    
-                game_array[i[0]][i[1]] = 0 
+
+                game_array[i[0]][i[1]] = 0
                 game_array[player_pos[0]][player_pos[1]] = 2
         return val
+
 
 def minimax_heuristic(game_array, level, nr, alfa, beta):
     global dynamic_depth
     try:
-        player_pos, bot_pos = positions(game_array) 
+        player_pos, bot_pos = positions(game_array)
     except:
-        if (level == "maxi"):
+        if level == "maxi":
             return -100
         else:
             return 100
 
-    step_and_block = StepBlock(0,0,0,0)    
-    bot_steppable= occupiable_spaces(game_array, bot_pos)
+    step_and_block = StepBlock(0, 0, 0, 0)
+    bot_steppable = occupiable_spaces(game_array, bot_pos)
     player_steppable = occupiable_spaces(game_array, player_pos)
 
-    #Heuristic: Winning routes get priority
-    if (len(bot_steppable)==0):
+    # Heuristic: Winning routes get priority
+    if len(bot_steppable) == 0:
         return -100
-    if (len(player_steppable)==0):
+    if len(player_steppable) == 0:
         return 100
 
     bot_blocks = occupiable_spaces_degree(game_array, bot_pos)
     player_blocks = occupiable_spaces_degree(game_array, player_pos)
-    #Heuristic: we look at the degree of every position the contestants can step into (as in, the number of possible positions, the positions we step into offer)
-    if (nr >= 2 + dynamic_depth//2):
-        return (sum(bot_blocks) - sum(player_blocks))
+    # Heuristic: we look at the degree of every position the contestants can step into (as in, the number of possible positions, the positions we step into offer)
+    if nr >= 2 + dynamic_depth // 2:
+        return sum(bot_blocks) - sum(player_blocks)
 
-    if (level == "maxi"):
+    if level == "maxi":
         val = -100
         for i in bot_steppable:
             game_array[bot_pos[0]][bot_pos[1]] = 0
-            game_array[i[0]][i[1]] = 3 
-            #Heuristic: we give the blockable tiles next to the player priority (as in the bot will usually block these tiles first)
+            game_array[i[0]][i[1]] = 3
+            # Heuristic: we give the blockable tiles next to the player priority (as in the bot will usually block these tiles first)
             contestant_blockable = copy.deepcopy(player_steppable)
             contestant_blockable = blockable_pos(game_array)
             contestant_blockable = list(set(contestant_blockable))
             for j in contestant_blockable:
                 game_array[j[0]][j[1]] = 1
-                result = minimax_heuristic(game_array, "mini", nr+1, alfa,beta)
+                result = minimax_heuristic(game_array, "mini", nr + 1, alfa, beta)
                 game_array[j[0]][j[1]] = 0
-                if (val<result):
+                if val < result:
                     val = result
-                    if (nr==0 and alfa >= 1000):
+                    if nr == 0 and alfa >= 1000:
                         game_array[j[0]][j[1]] = 0
-                        game_array[i[0]][i[1]] = 0 
+                        game_array[i[0]][i[1]] = 0
                         game_array[bot_pos[0]][bot_pos[1]] = 3
-                        return (i[0],i[1]), (j[0],j[1])
-                    step_and_block = replace(step_and_block, step_x = i[0], step_y = i[1], block_x = j[0], block_y = j[1])
-                #Heuristic: Alfa-beta cutting
-                alfa = max(alfa,val)
-                if (val >= beta):
-                    break  
+                        return (i[0], i[1]), (j[0], j[1])
+                    step_and_block = replace(
+                        step_and_block,
+                        step_x=i[0],
+                        step_y=i[1],
+                        block_x=j[0],
+                        block_y=j[1],
+                    )
+                # Heuristic: Alfa-beta cutting
+                alfa = max(alfa, val)
+                if val >= beta:
+                    break
 
-            game_array[i[0]][i[1]] = 0 
+            game_array[i[0]][i[1]] = 0
             game_array[bot_pos[0]][bot_pos[1]] = 3
-        if (nr==0 and val >= 1):
-            return (step_and_block.step_x, step_and_block.step_y), (step_and_block.block_x, step_and_block.block_y)
+        if nr == 0 and val >= 1:
+            return (step_and_block.step_x, step_and_block.step_y), (
+                step_and_block.block_x,
+                step_and_block.block_y,
+            )
         else:
-            if (nr==0):
-                return (i[0],i[1]),(j[0],j[1])
+            if nr == 0:
+                return (i[0], i[1]), (j[0], j[1])
         return val
     else:
         val = 100
-        if (level == "mini"):
+        if level == "mini":
             for i in player_steppable:
                 game_array[player_pos[0]][player_pos[1]] = 0
-                game_array[i[0]][i[1]] = 2 
-                contestant_blockable = blockable_pos(game_array) 
+                game_array[i[0]][i[1]] = 2
+                contestant_blockable = blockable_pos(game_array)
                 for j in contestant_blockable:
                     game_array[j[0]][j[1]] = 1
-                    result = minimax_heuristic(game_array, "maxi", nr+1,alfa,beta)
+                    result = minimax_heuristic(game_array, "maxi", nr + 1, alfa, beta)
                     game_array[j[0]][j[1]] = 0
                     val = min(val, result)
-                    #Heuristic: Alfa-beta cutting
-                    beta = min(beta,val)
-                    if (alfa>=val):
+                    # Heuristic: Alfa-beta cutting
+                    beta = min(beta, val)
+                    if alfa >= val:
                         break
-                game_array[i[0]][i[1]] = 0 
+                game_array[i[0]][i[1]] = 0
                 game_array[player_pos[0]][player_pos[1]] = 2
         return val
